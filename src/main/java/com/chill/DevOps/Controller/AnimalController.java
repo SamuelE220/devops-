4e614919ef89b4b3a23623118089d7bf8870f92c
samyu <samyueru312@gmail.com>
2025-07-12 10:02:57 -0600

    Proyecto

---
+++ src/main/java/com/chill/DevOps/Controller/AnimalController.java
@@ -0,0 +1,9 @@
+package com.chill.DevOps.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnimalController {
+
+    //Aquí irá un metodo :D
+}
